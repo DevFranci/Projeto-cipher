a0c72590f1209a95724f6ce07955d42b6da1e98b
DevFranci <devfranciele@gmail.com>
2022-02-08 02:50:19 -0300

    Updates

--- cipher.js
+++ cipher.js
@@ -1,5 +1,59 @@
 const cipher = {
-  // ...
+  encode: function encode(offset, message) {
+    if (typeof offset != 'number' || typeof message != 'string') {
+      throw new TypeError();
+    }
+    console.log('nãopode')
+    let asciiCode = 0;
+    let finalString = "";
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].charCodeAt() == 32) {
+        finalString += " ";
+      }
+      else if (message[i].charCodeAt() >= 65 && message[i].charCodeAt() <= 90) {
+        asciiCode = mod((message[i].charCodeAt() - 65 + offset), 26) + 65;
+        finalString += String.fromCharCode(asciiCode);
+      }
+      else if (message[i].charCodeAt() >= 97 && message[i].charCodeAt() <= 122) {
+        asciiCode = mod((message[i].charCodeAt() - 97 + offset), 26) + 97;
+        finalString += String.fromCharCode(asciiCode);
+      }
+      else {
+        finalString += message[i];
+      }
+
+    }
+    return finalString;
+  },
+  decode: function decode(offset, message) {
+    if (typeof offset != 'number' || typeof message != 'string') {
+      throw new TypeError();
+    }
+
+    let asciiCode = 0;
+    let finalString = "";
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].charCodeAt() == 32) {
+        finalString += " ";
+      }
+      else if (message[i].charCodeAt() >= 65 && message[i].charCodeAt() <= 90) {
+        asciiCode = mod((message[i].charCodeAt() - 65 + (offset * -1)), 26) + 65;
+        finalString += String.fromCharCode(asciiCode);
+      }
+      else if (message[i].charCodeAt() >= 97 && message[i].charCodeAt() <= 122) {
+        asciiCode = mod((message[i].charCodeAt() - 97 + (offset * -1)), 26) + 97;
+        finalString += String.fromCharCode(asciiCode);
+      }
+      else {
+        finalString += message[i];
+      }
+    }
+    return finalString;
+  },
+
 };
 
+function mod(n, m) {
+  return ((n % m) + m) % m;
+}
 export default cipher;
--- index.js
+++ index.js
@@ -1,92 +1,63 @@
-//import cipher from './cipher.js';
+import cipher from './cipher.js';
+console.log(cipher)
 
-console.log(cipher);
-function mod(n, m) {
-    return ((n % m) + m) % m;
+window.getText = function getText() {
+
+  var inputDeslocamento = document.getElementById("key");
+  var stringDeslocamento = inputDeslocamento.value;
+  const deslocamento = parseInt(stringDeslocamento)
+  if (isNaN(deslocamento)) {
+    alert("O deslocamento deve ser apenas números");
+    return;
   }
-  
-  
-   function encode(offset, message) {
-    
-    let asciiCode = 0;
-    let finalString = "";
-    for (let i = 0; i < message.length; i++) {
-      if (message[i].charCodeAt() == 32) {
-        finalString += " ";
-      }
-      else if (message[i].charCodeAt() >= 65 && message[i].charCodeAt() <= 90) {
-        asciiCode = mod((message[i].charCodeAt() - 65 + offset), 26) + 65;
-        finalString += String.fromCharCode(asciiCode);
-      }
-      else if (message[i].charCodeAt() >= 97 && message[i].charCodeAt() <= 122) {
-        asciiCode = mod((message[i].charCodeAt() - 97 + offset), 26) + 97;
-        finalString += String.fromCharCode(asciiCode);
-      }
-      else {
-        finalString += message[i];
-      }
-      
-    }
-    return finalString;
-  } 
-  
-  function getText() {
-    
-    var inputDeslocamento = document.getElementById("key");
-    var stringDeslocamento = inputDeslocamento.value;
-    const deslocamento = parseInt (stringDeslocamento)
-    let message = document.getElementById("userInput");
-    const final = encode(deslocamento, message.value);
-    message.value = final;
-    message.select();
-    message = document.execCommand("copy");
+  let message = document.getElementById("userInput");
+  const final = cipher.encode(deslocamento, message.value);
+  message.value = final;
+  message.select();
+  message = document.execCommand("copy");
+};
+
+window.getText2 = function getText2() {
+  const offset = parseInt(document.getElementById("keyD").value);
+  if (isNaN(offset)) {
+    alert("O deslocamento deve ser apenas números");
+    return;
   }
-  
-  function decode(offset, message) {
-    
-    let asciiCode = 0;
-    let finalString = "";
-    for (let i = 0; i < message.length; i++) {
-      if (message[i].charCodeAt() == 32) {
-        finalString += " ";
-      }
-      else if (message[i].charCodeAt() >= 65 && message[i].charCodeAt() <= 90) {
-        asciiCode = mod((message[i].charCodeAt() - 65 + (offset * -1 )), 26) + 65;
-        finalString += String.fromCharCode(asciiCode);
-      }
-      else if (message[i].charCodeAt() >= 97 && message[i].charCodeAt() <= 122) {
-        asciiCode = mod((message[i].charCodeAt() - 97 + (offset * -1 )), 26) + 97;
-        finalString += String.fromCharCode(asciiCode);
-      }
-      else {
-        finalString += message[i];
-      }
-    }
-    return finalString;
-  }
-    
-    
-  
-  function getText2() {
-    const offset = parseInt(document.getElementById("keyD").value);
-    let message = document.getElementById("userInputD");
-    const final = decode(offset, message.value);
-  
-    message.value = final;
-    message.select();
-    message = document.execCommand("copy");
-  }
-  
-  function limparElementoCodificar() {
-    var key = document.getElementById("key")
-    var userInput = document.getElementById("userInput")
-    key.value = "" 
-    userInput.value = ""
-    
-  }
-  function limparElementoDecodificar() {
-    var keyD = document.getElementById("keyD")
-    var userInputD = document.getElementById("userInputD")
-    keyD.value = ""
-    userInputD.value = ""
-  }
+  let message = document.getElementById("userInputD");
+  const final = cipher.decode(offset, message.value);
+
+  message.value = final;
+  message.select();
+  message = document.execCommand("copy");
+};
+
+window.limparElementoCodificar = function limparElementoCodificar() {
+  var key = document.getElementById("key")
+  var userInput = document.getElementById("userInput")
+  key.value = ""
+  userInput.value = ""
+
+};
+
+window.limparElementoDecodificar = function limparElementoDecodificar() {
+  var keyD = document.getElementById("keyD")
+  var userInputD = document.getElementById("userInputD")
+  keyD.value = ""
+  userInputD.value = ""
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
