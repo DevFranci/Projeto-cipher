c7c1e454645f5bb080a8a632a592df912c97487b
DevFranci <devfranciele@gmail.com>
2022-02-15 07:14:50 -0300

    Refatoraçao do projeto

--- src/cipher.js
+++ src/cipher.js
@@ -1,59 +1,91 @@
+const charCodes = {
+  A: 65, //Código ASCII da letra A maúscula
+  Z: 90,
+  a: 97,  
+  z: 122, // Código ASCII da letra z minuscúla
+  wrapSize: 26 //Tamanho do alfabeto
+}
+
+const encode = (offset, message) => {
+  if(typeof offset != 'number' || typeof message != 'string') {
+      throw new TypeError();
+  }
+
+  let asciiCode = 0;
+  let finalString = "";
+
+  for(let i = 0; i < message.length; i++) {
+      asciiCode = message[i].charCodeAt();
+
+      if(asciiCode >= charCodes.a && asciiCode <= charCodes.z) {
+          asciiCode = encodeLowerCase(offset, asciiCode);
+      } else if(asciiCode >= charCodes.A && asciiCode <= charCodes.Z) {
+          asciiCode = encodeUpperCase(offset, asciiCode);
+      }
+
+      finalString += String.fromCharCode(asciiCode);
+  }
+
+
+  return finalString;
+}
+
+const encodeLowerCase = (offset, asciiCode) => {
+  let part1 = (asciiCode - charCodes.a + offset);  // ele traz para o zero para facilitar e soma o deslocamento intervalo[0,25] total da 26. 
+  let part2 = part1 % charCodes.wrapSize; // ele usa o mod para tratar os casos em que a soma ultrapasssa o codigo ascii do Z. O mod eh o resto da divisao e vai gerar a letra correta.
+  let part3 = part2 + charCodes.a; // somando o valor do codigo inicial para conseguir a letra correta.
+  return  part3;
+}
+const encodeUpperCase = (offset, asciiCode) => {
+  let part1 = (asciiCode - charCodes.A + offset);  // ele traz para o zero para facilitar e soma o deslocamento intervalo[0,25] total da 26. 
+  let part2 = part1 % charCodes.wrapSize; // ele usa o mod para tratar os casos em que a soma ultrapasssa o codigo ascii do Z. O mod eh o resto da divisao e vai gerar a letra correta.
+  let part3 = part2 + charCodes.A; // somando o valor do codigo inicial para conseguir a letra correta.
+  return  part3;
+}
+const decode = (offset, message) => {
+  if(typeof offset != 'number' || typeof message != 'string') {
+      throw new TypeError();
+  }
+
+  let asciiCode = 0;
+  let finalString = "";
+
+  for(let i = 0; i < message.length; i++) {
+      asciiCode = message[i].charCodeAt();
+
+      if(asciiCode >= charCodes.a && asciiCode <= charCodes.z) {
+          asciiCode = decodeLowerCase(offset, asciiCode);
+      } else if(asciiCode >= charCodes.A && asciiCode <= charCodes.Z) {
+          asciiCode = decodeUpperCase(offset, asciiCode);
+      }
+
+      finalString += String.fromCharCode(asciiCode);
+  }
+
+  return finalString;
+}
+const decodeLowerCase = (offset, asciiCode) => {
+  let part1 = (asciiCode - charCodes.a - offset); // Ele subtrai o código inicial para trazer entre o e 25, e depois subtrai o deslocamento para achar e mensagem decodificada
+  let part2 = part1 % charCodes.wrapSize; // 
+  let part3 = part2 + charCodes.wrapSize; // 
+  let part4 = part3 % charCodes.wrapSize; // 
+  let part5 = part4 + charCodes.a; // voltando para o codigo ASCII
+  return part5;
+}
+
+const decodeUpperCase = (offset, asciiCode) => {
+  let part1 = (asciiCode - charCodes.A - offset); // normalizar entre 0 e 25
+  let part2 = part1 % charCodes.wrapSize; // 
+  let part3 = part2 + charCodes.wrapSize; // 
+  let part4 = part3 % charCodes.wrapSize; // caso de letras normais
+  let part5 = part4 + charCodes.A; // voltando para o codigo ASCII
+  return part5;
+}
+
 const cipher = {
-  encode: function encode(offset, message) {
-    if (typeof offset != 'number' || typeof message != 'string') {
-      throw new TypeError();
-    }
-    console.log('nãopode')
-    let asciiCode = 0;
-    let finalString = "";
-    for (let i = 0; i < message.length; i++) {
-      if (message[i].charCodeAt() == 32) {
-        finalString += " ";
-      }
-      else if (message[i].charCodeAt() >= 65 && message[i].charCodeAt() <= 90) {
-        asciiCode = mod((message[i].charCodeAt() - 65 + offset), 26) + 65;
-        finalString += String.fromCharCode(asciiCode);
-      }
-      else if (message[i].charCodeAt() >= 97 && message[i].charCodeAt() <= 122) {
-        asciiCode = mod((message[i].charCodeAt() - 97 + offset), 26) + 97;
-        finalString += String.fromCharCode(asciiCode);
-      }
-      else {
-        finalString += message[i];
-      }
-
-    }
-    return finalString;
-  },
-  decode: function decode(offset, message) {
-    if (typeof offset != 'number' || typeof message != 'string') {
-      throw new TypeError();
-    }
-
-    let asciiCode = 0;
-    let finalString = "";
-    for (let i = 0; i < message.length; i++) {
-      if (message[i].charCodeAt() == 32) {
-        finalString += " ";
-      }
-      else if (message[i].charCodeAt() >= 65 && message[i].charCodeAt() <= 90) {
-        asciiCode = mod((message[i].charCodeAt() - 65 + (offset * -1)), 26) + 65;
-        finalString += String.fromCharCode(asciiCode);
-      }
-      else if (message[i].charCodeAt() >= 97 && message[i].charCodeAt() <= 122) {
-        asciiCode = mod((message[i].charCodeAt() - 97 + (offset * -1)), 26) + 97;
-        finalString += String.fromCharCode(asciiCode);
-      }
-      else {
-        finalString += message[i];
-      }
-    }
-    return finalString;
-  },
-
+  encode: encode, 
+  decode: decode, 
 };
 
-function mod(n, m) {
-  return ((n % m) + m) % m;
-}
+
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import cipher from './cipher.js';
-console.log(cipher)
+import cipher from "./cipher.js";
 
-window.getText = function getText() {
+document.getText = () => {
+  let inputOffset = document.getElementById("key");
+  let stringOffset = inputOffset.value;
+  const offset = parseInt(stringOffset);
 
-  var inputDeslocamento = document.getElementById("key");
-  var stringDeslocamento = inputDeslocamento.value;
-  const deslocamento = parseInt(stringDeslocamento)
-  if (isNaN(deslocamento)) {
+  if (isNaN(offset)) {
     alert("O deslocamento deve ser apenas números");
     return;
   }
+
   let message = document.getElementById("userInput");
-  const final = cipher.encode(deslocamento, message.value);
+  const final = cipher.encode(offset, message.value);
   message.value = final;
   message.select();
   message = document.execCommand("copy");
 };
 
-window.getText2 = function getText2() {
+document.getText2 = () => {
   const offset = parseInt(document.getElementById("keyD").value);
   if (isNaN(offset)) {
     alert("O deslocamento deve ser apenas números");
@@ -31,33 +31,16 @@ window.getText2 = function getText2() {
   message = document.execCommand("copy");
 };
 
-window.limparElementoCodificar = function limparElementoCodificar() {
-  var key = document.getElementById("key")
-  var userInput = document.getElementById("userInput")
-  key.value = ""
-  userInput.value = ""
-
+document.clearEncode = () => {
+  var key = document.getElementById("key");
+  var userInput = document.getElementById("userInput");
+  key.value = "";
+  userInput.value = "";
 };
 
-window.limparElementoDecodificar = function limparElementoDecodificar() {
-  var keyD = document.getElementById("keyD")
-  var userInputD = document.getElementById("userInputD")
-  keyD.value = ""
-  userInputD.value = ""
+document.clearDecode = () => {
+  var keyD = document.getElementById("keyD");
+  var userInputD = document.getElementById("userInputD");
+  keyD.value = "";
+  userInputD.value = "";
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
